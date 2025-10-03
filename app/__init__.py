9262d84808d1c1189e60ba858fb46531c56ff87d
dawn-boy <curiouslittleboy@pm.me>
2025-10-03 15:07:32 +0530

    Celery, redis and flask. Up and running

---
+++ app/__init__.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from celery import Celery, Task
+from flask_htmx import HTMX
+
+def create_app():
+    app = Flask(__name__)
+    htmx = HTMX(app)
+
+    app.config.from_mapping(
+        CELERY=dict(
+            broker_url="redis://redis:6379/0",
+            result_backend="redis://redis:6379/0",
+            task_serializer='pickle',
+            result_serializer='pickle',
+            accept_content=['pickle', 'json'],
+        )
+    )
+    celery_init_app(app)
+
+    from . import routes
+    app.register_blueprint(routes.bp)
+
+    return app
+
+def celery_init_app(app: Flask) -> Celery:
+    class FlaskTask(Task):
+        def __call__(self, *args: object, **kwargs: object) -> object:
+            with app.app_context():
+                return self.run(*args, **kwargs)
+
+    celery_app = Celery(app.name, task_cls=FlaskTask)
+    celery_app.config_from_object(app.config["CELERY"])
+    celery_app.set_default()
+    app.extensions["celery"] = celery_app
+    return celery_app
