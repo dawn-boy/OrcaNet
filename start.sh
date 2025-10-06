801f09e1c95c354ac96740956f59292249c3bda5
dawn-boy <curiouslittleboy@pm.me>
2025-10-06 23:34:40 +0530

    added run.sh6

--- start.sh
+++ start.sh
@@ -2,11 +2,11 @@
 set -e
 
 # 1. Ensure the static directory exists and is empty
-mkdir -p /app/static
-rm -rf /app/static/*
+mkdir -p app/static
+rm -rf app/static/*
 
 # 2. Copy the prebuilt assets into the volume-mounted /app/static
-cp -R /app/static_assets_source/. /app/static/
+cp -R app/static_assets_source/. app/static/
 
 # 3. Execute the final command (CMD)
 exec "$@"
